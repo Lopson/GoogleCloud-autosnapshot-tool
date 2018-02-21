040ac1b546ce70a6fcd8ee239fcee0ba6dd72795
jmg87 <jmiralles@tuta.io>
2018-02-21 14:33:04 +0100

    Update autosnapshot.sh

--- autosnapshot.sh
+++ autosnapshot.sh
@@ -5,7 +5,8 @@ gcloud compute disks list --format='value(name,zone)'| while read DISK_NAME ZONE
 gcloud compute disks snapshot $DISK_NAME --snapshot-names autosnapshot-${DISK_NAME:0:15}-$(date "+%Y-%m-%d-%s") --zone $ZONE
 done
 
-#Delete the snaphots that are a week old
+# Set the expiration date so snapshots older than a week will be deleted
+
 EXPIRED=$(date -d "-7 days" +%Y-%m-%d)
 echo "##################################"
 echo " \n"
@@ -16,6 +17,8 @@ echo " \n"
 #Beware! Without further filtering it's deleting ALL the snapshots present, not only the ones created by the script. In case
 #you want to limit the snapshot cleanup to only the ones created by it swap the "--filter="creationTimesTamp<$EXPIRED" below
 #with this one: --filter="creationTimesTamp<$EXPIRED AND name~"autosnapshot*"
+
+# Delete the snapshots
 
 gcloud compute snapshots list --filter="creationTimesTamp<$EXPIRED" --uri | while read SNAPSHOT_URI; do
         echo "Deleting snapshot: $SNAPSHOT_URI"
